fff4052adc469f34f45ac5ad2e23b4505980cb98
emshano <emira@eshano.com>
2024-09-30 23:42:42 -0400

    added tutoring page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import '../src/assets/tailwind.css'
 import Home from "./Home";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Background from "./components/Background";
 import Projectss from "./components/Projectss";
+import Tutoring from './components/Tutoring';
+
 
 const App = () => {
   return(
     <div className="bg-gradient-to-r from-green-300 to-blue-400">
       <Background/>
+      
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projectss/>}/>
+        <Route path="/tutoring" element={<Tutoring />} />
       </Routes>
+      
+
     </div>
   )
 
---
+++ src/components/Tutoring.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Tutoring = () => {
+    return (
+        <div className="contentLayer relative flex flex-col items-center justify-around min-h-screen gap-x-56 xl:gap-x-80 w-full">
+            <h1 className="text-6xl mt-24 font-bold text-white">Tutoring</h1>
+            <p>Coming soon...</p>
+        </div>
+    )
+}
+
+export default Tutoring;
