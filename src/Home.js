e0f164db37169c30f9a1d30954dbb82f2269da2f
emshano <emira@eshano.com>
2024-10-02 01:13:07 -0400

    changed relative path links to absolute paths

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,8 @@ const Home = ({ color }) => {
         <div className="flex flex-col h-full ">
             <Banner color={color}/>
             <div className="links flex flex-col ml-2 items-start text-4xl">
-                <a href="/projects">→ projects</a>
-                <a href="/tutoring">→ tutoring</a>
+                <a href="https://eshano.com/projects">→ projects</a>
+                <a href="https://eshano.com/tutoring">→ tutoring</a>
                 <a href="mailto:emira@eshano.com">→ contact</a>
                 <a href="https://www.linkedin.com/in/emirashano/">→ linkedIn</a>
             </div>
