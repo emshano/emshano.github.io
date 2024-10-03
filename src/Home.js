530792a3cb7c3c3e6e53762333c29986ad9fc70e
emshano <emira@eshano.com>
2024-10-02 21:05:09 -0400

    fixed anchor tag issue with projects and tutoring page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import Banner from "./components/Banner";
+import { Link } from "react-router-dom";
 
 
 const Home = ({ color }) => {
@@ -8,8 +9,8 @@ const Home = ({ color }) => {
         <div className="flex flex-col h-full ">
             <Banner color={color}/>
             <div className="links flex flex-col ml-2 items-start text-4xl">
-                <a href="https://eshano.com/projects">→ projects</a>
-                <a href="https://eshano.com/tutoring">→ tutoring</a>
+                <Link to="./projects">→ projects</Link>
+                <Link to="./tutoring">→ tutoring</Link>
                 <a href="mailto:emira@eshano.com">→ contact</a>
                 <a href="https://www.linkedin.com/in/emirashano/">→ linkedIn</a>
             </div>
