98db495044e6a8e40b7701fa485fa82abf4654c6
emshano <emira@eshano.com>
2024-10-03 23:06:39 -0400

    updated project list

--- src/projectList.js
+++ src/projectList.js
@@ -1,6 +1,3 @@
-import earworm from "./images/ear.png"
-import chess from "./images/bbchess.png"
-import flix from "./images/flixter.png"
 import cpp from "./images/c++.svg"
 import ract from "./images/react.svg"
 import sw from "./images/swift.svg"
@@ -9,10 +6,15 @@ import typer from "./images/type.png"
 const projectList = [
   {
     title: 'earworm',
-    description: 'Compiles the Billboard 200 Year End charts ' +
-        'from 1970-2020 with genre analysis.',
+    description: 'Compiles the Billboard 200 Year End charts from 1970-2020 with genre analysis.',
     lang: ract,
     link: 'https://earwrm.io'
+  }, {
+    title: 'myHub',
+    description: "Smart mirror dashboard. Displays time, date, and live NYC subway station arrival/departure times.",
+        lang: ract,
+        link: 'https://github.com/emshano/myHub'
+
   }, {
     title: 'flixter',
     description: 'Displays the currently playing movies across the country. Uses the MovieDB API.',
@@ -24,12 +26,6 @@ const projectList = [
     description: 'A simplified commandline chess game written in C++.',
         lang: cpp,
         link: 'https://github.com/emshano/bb_chess'
-
-  }   , {
-    title: 'butterfingers',
-    description: "No frills type test that measures WPM and accuracy. Built with ReactJS",
-        lang: ract,
-        link: 'https://github.com/emshano/butterfingers'
 
   }
 ]
